6cfb5c6c0dac413f4318a040950a341f84e74d0f
Larry Laski <larry.laski@gmail.com>
2015-06-30 18:03:02 -0400

    init commit

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Home = React.createClass({
+
+	render: function() {
+		return (
+			<div>
+				<h2 className="text-center">
+					Search by Github Username Above
+				</h2>
+			</div>
+		);
+	}
+
+});
+
+module.exports = Home;
